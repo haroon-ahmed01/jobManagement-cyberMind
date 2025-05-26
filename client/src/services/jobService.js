7e83ab27cdcb824b4e9902c22e331b79d0cb27ce
haroon-ahmed01 <haroonahmed5652@gmail.com>
2025-05-26 15:56:42 +0530

    updated frontend for deployment

--- client/src/services/jobService.js
+++ client/src/services/jobService.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000';
+
 
 const jobService = {
   getJobs: async (filters = {}) => {
