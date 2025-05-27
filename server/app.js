1a6d09f1b319d7c84768cd8562d850e19d5574a0
haroon-ahmed01 <haroonahmed5652@gmail.com>
2025-05-27 21:39:36 +0530

    Fix CORS issue

--- server/app.js
+++ server/app.js
@@ -8,7 +8,7 @@ const app = express();
 
 
 app.use(cors({
-  origin: ["https://jobmanagement-cm.onrender.com"],
+  origin: ["http://localhost:5173", "https://jobmanagement-cm.onrender.com"],
   credentials: true
 }));
 app.use(express.json());
